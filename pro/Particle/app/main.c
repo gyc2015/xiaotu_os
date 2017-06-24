f2dbf8f2129ab7fc870ae2e28f0df68c8b6114db
GAO Yichao <gaoyichao2013@126.com>
2017-06-24 15:39:19 +0800

    sdio

--- pro/Particle/app/main.c
+++ pro/Particle/app/main.c
@@ -2,6 +2,7 @@
 #include <xtos.h>
 #include <led.h>
 #include <uart.h>
+#include <sdio.h>
 #include <fakei2c.h>
 #include <eeprom.h>
 #include <mpu6050.h>
@@ -36,11 +37,15 @@ void taskb() {
 uint8 txBuf[10] = { 'A', 'b', 'C', 'd', 'E', 'f', 'G', 'h', 'I', 'j' };
 uint8 rxBuf[10] = { 0,1,2,3,4,5,6,7,8,9 };
 
+struct sd_card card;
+uint8 readbuf[1024];
+
 int main(void) {
     led_init();
     usart3_init(115200);
     eeprom_init();
     mpu6050_init();
+    sdio_init(&card);
 
     config_interruts();
 
@@ -51,6 +56,21 @@ int main(void) {
     //eeprom_write_bytes(txBuf, 10, 0);
     eeprom_read_bytes(rxBuf, 10, 0);
     uart_send_bytes(USART3, rxBuf, 10);
+
+    uart_send_str(USART3, "\r\nG.Y.C\r\n");
+    for (int i = 0; i < 1024; i++)
+        readbuf[i] = '0';
+
+    if (SDE_OK != sdio_read_multiblock(&card, 0, readbuf, 2))
+        uart_send_str(USART3, "\r\nhehe\r\n");
+    if (SDE_OK != sdio_read_finished(&card))
+        uart_send_str(USART3, "\r\nhaha\r\n");
+    while (SDE_OK != sdio_expect_card_state(&card, SD_CS_TRAN))
+        uart_send_str(USART3, "\r\nxixi\r\n");
+
+    uart_send_str(USART3, "\r\n====\r\n");
+    uart_send_bytes(USART3, readbuf, 1024);
+
 
     xtos_init(168000);
     xtos_init_task_struct(&taskA, taska, &taskA_Stk[TASKA_STK_SIZE - 1], 0);
